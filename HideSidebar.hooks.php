2a3020a4a564b96a1bece112f7545716cc765bee
paladox <paladox@users.noreply.github.com>
2021-06-09 19:32:05 +0100

    Stop overriding $bar

--- HideSidebar.hooks.php
+++ HideSidebar.hooks.php
@@ -1,19 +1,18 @@
 <?php
 class HideSidebarHooks {
-    public static function efHideSidebar($skin, &$bar) {
+    public static function efHideSidebar( $skin, &$bar ) {
         global $wgUser;
         // Hide sidebar for anonymous users
-        if (!$wgUser->isLoggedIn()) {
-            $url = Title::makeTitle(NS_SPECIAL, 'UserLogin')->getLocalUrl();
-            $bar = array(
-                'navigation' => array(
-                    array('text'   => 'Login',
-                          'href'   => $url,
-                          'id'     => 'n-login',
-                          'active' => '')
-                )
-            );
+        if ( !$wgUser->isLoggedIn() ) {
+            $url = Title::makeTitle( NS_SPECIAL, 'UserLogin' )->getLocalUrl();
+            $bar['navigation'][] = [
+                'text'   => 'Login',
+                'href'   => $url,
+                'id'     => 'n-login',
+                'active' => '',
+            ];
         }
+
         return true;
     }
 }
